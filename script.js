d848decf14dfc40220a80f79cab95ece7a8686ba
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-04 00:47:03 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-//complete this code
-class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  /**
-   * @param {any} age
-   */
-  set age(age) {
-    this._age = age;
-  }
+class Person{
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age = age;
+	}
 }
 
 class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying`);
-  }
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+	
 }
 
 class Teacher extends Person {
-  teach() {
-    console.log(`${this.name} is teaching`);
-  }
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
 }
 
 // Do not change the code below this line
