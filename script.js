db4512207423ce0cce4ef0c20c5ce2514c06dc42
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-04 00:30:04 +0545

    created a submission

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ class Person{
 }
 
 class Student extends Person {
-	constructor(name)
-	{
-		super(name);
-	}
 	study(){
 		console.log(`${this.name} is studying`);
 	}
@@ -27,25 +23,10 @@ class Student extends Person {
 }
 
 class Teacher extends Person {
-	constructor(name)
-	{
-		super(name);
-	}
 	teach(){
 		console.log(`${this.name} is teaching`);
 	}
 }
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
-
-person.age = 30;
-console.log(person.age); // Output: 30
-
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
